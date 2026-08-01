2b2c701a18db5d1afd9a4a02a45b5b75ffe4b561
mohinikardam19 <mohinikardam064@gmail.com>
2026-08-01 20:42:23 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const form = document.querySelector("form");
+
+    if(form){
+
+        form.addEventListener("submit", function(event){
+
+            event.preventDefault();
+
+            alert("Thank you! Your message has been submitted successfully.");
+
+            form.reset();
+
+        });
+
+    }
+
+
+});
+function toggleTheme() {
+    document.body.classList.toggle("dark-mode");
+} 
